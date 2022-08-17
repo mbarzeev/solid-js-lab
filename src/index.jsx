43a7aa5c05a3f8f3d32feed0ea61f8f1f61b81f5
Matti Bar-Zeev <flashmattic@gmail.com>
2022-08-17 18:43:11 +0300

    chore: Use the dev-tools Locator feature

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 /* @refresh reload */
 import 'solid-devtools';
 import {render} from 'solid-js/web';
-
+import {useLocatorPlugin} from 'solid-devtools';
+import {DEV} from 'solid-js';
+import {isServer} from 'solid-js/web';
 import './index.css';
 import App from './App';
+
+if (DEV && !isServer) {
+    console.log('In DEV mode');
+    useLocatorPlugin({
+        targetIDE: 'vscode',
+    });
+}
 
 render(() => <App />, document.getElementById('root'));
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import devtoolsPlugin from '@solid-devtools/transform';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
+    plugins: [
+        devtoolsPlugin({
+            jsxLocation: true,
+            name: true,
+        }),
+        solidPlugin(),
+    ],
+    build: {
+        target: 'esnext',
+        polyfillDynamicImport: false,
+    },
 });
